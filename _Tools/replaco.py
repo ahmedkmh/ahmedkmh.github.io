792a40db0c2b63c5e40713653f37a315a8da22fc
Ahmed Khalid Mohamed <121353760+ahmedkmh@users.noreply.github.com>
2023-01-09 23:38:57 +0200

    Add files via upload

---
+++ _Tools/replaco.py
@@ -0,0 +1,13 @@
+fileos = ["News", "Pictures", "Videos"]
+for name in fileos:
+    #input file
+    fin = open(f"../{name}/index.html", "rt")
+    #output file to write the result to
+    fout = open(f"../home/{name}/index.html", "wt")
+    #for each line in the input file
+    for line in fin:
+        #read replace the string and write to output file
+        fout.write(line)
+    #close input and output files
+    fin.close()
+    fout.close()
