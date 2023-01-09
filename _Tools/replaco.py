20f368bc1342f57b44474da2edf76035b5d2c607
Ahmed Khalid Mohamed <121353760+ahmedkmh@users.noreply.github.com>
2023-01-09 23:48:17 +0200

    Update replaco.py

--- _Tools/replaco.py
+++ _Tools/replaco.py
@@ -1,9 +1,9 @@
 files = ["News", "Pictures", "Videos"]
 for name in files:
     #input file
-    fin = open(f"/{name}/index.html", "rt")
+    fin = open(f"{name}/index.html", "rt")
     #output file to write the result to
-    fout = open(f"/home/{name}/index.html", "wt")
+    fout = open(f"home/{name}/index.html", "wt")
     #for each line in the input file
     for line in fin:
         #read replace the string and write to output file
